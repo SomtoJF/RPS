4460e64188b6307ae27a82022311d377f6bca00f
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-06-25 21:54:08 -0800

    Feature: Play one round of RPS with the computer onclick

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 let playerSelection;
 let playerScore = 0, computerScore = 0;
 let selectionArr = ['rock','paper','scissors'];
@@ -49,3 +50,73 @@ function game(){
     }
 }
 console.log(game());
+*/
+let computerSelectionVar;
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let result = document.getElementById('results');
+let playerScore = document.getElementById('playerScore');
+let computerScore = document.getElementById('computerScore');
+console.log(rock);
+
+let selectionArr = ['rock','paper','scissors'];
+
+function computerSelection(){
+    const randomIndex = Math.floor(Math.random() * 3);
+    return selectionArr[randomIndex];
+}
+rock.addEventListener('click',function (){
+    computerSelectionVar = computerSelection();
+    
+    if(computerSelectionVar == 'rock'){
+        result.textContent = 'Rock vs Rock, Its a tie.';
+        
+    }
+    else if(computerSelectionVar == 'paper'){
+        result.textContent = 'Rock vs Paper, Computer wins this round.';
+
+    }
+    else if(computerSelectionVar == 'scissors'){
+        result.textContent = 'Rock vs Scissors, You win this round.';
+
+    }
+    
+});
+
+paper.addEventListener('click',function (){
+    computerSelectionVar = computerSelection();
+
+    if(computerSelectionVar == 'rock'){
+        result.textContent = 'Paper vs Rock, You win this round.';
+
+    }
+    else if(computerSelectionVar == 'paper'){
+        result.textContent = 'Paper vs Paper, Its a tie.';
+
+    }
+    else if(computerSelectionVar == 'scissors'){
+        result.textContent = 'Paper vs Scissors, Computer wins this round.';
+
+    }
+    
+});
+
+scissors.addEventListener('click',function (){
+    computerSelectionVar = computerSelection();
+    
+    if(computerSelectionVar == 'rock'){
+        result.textContent = 'Scissors vs Rock, Computer wins this round.';
+
+    }
+    else if(computerSelectionVar == 'paper'){
+        result.textContent = 'Scissors vs Paper, You win this round.';
+
+    }
+    else if(computerSelectionVar == 'scissors'){
+        result.textContent = 'Scissors vs Scissors, Its a tie.';
+
+    }
+    
+});
+
